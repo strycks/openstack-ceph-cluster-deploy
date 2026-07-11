3d7c9a10076130628be210032e285c527b8b6141
strycks <ngochanguyenthe123@gmail.com>
2026-07-11 17:08:44 +0700

    feat: add hosts for cephadm-ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,16 +18,28 @@ resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
     openstack_nodes = {
       for k, v in libvirt_domain.vm :
-      v.name => try(v.network_interface[0].addresses[0], "No IP available yet.")
+      trimprefix(v.name, "${var.name_prefix}-") => try(v.network_interface[0].addresses[0], "No IP available yet.")
       if length(regexall(".*openstack.*", v.name)) > 0
     }
 
     ceph_nodes = {
       for k, v in libvirt_domain.vm :
-      v.name => try(v.network_interface[0].addresses[0], "No IP available yet.")
+      trimprefix(v.name, "${var.name_prefix}-") => try(v.network_interface[0].addresses[0], "No IP available yet.")
       if length(regexall(".*ceph.*", v.name)) > 0
     }
   })
 
   filename = "${path.module}/../ansible/inventory.ini"
+}
+
+resource "local_file" "ceph_inventory" {
+  content = templatefile("${path.module}/templates/ceph_inventory.tftpl", {
+    ceph_nodes = [
+      for vm in libvirt_domain.vm : {
+        name = trimprefix(vm.name, "${var.name_prefix}-")
+        mgmt_ip = try(vm.network_interface[0].addresses[0], "No IP available yet.")
+      } if length(regexall(".*ceph.*", vm.name)) > 0
+    ]
+  })
+  filename = "${path.module}/../ansible/ceph_inventory.ini"
 }
